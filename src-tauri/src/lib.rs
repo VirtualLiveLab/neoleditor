75987ef3104fb0c858e3622fd2ae988c5135b010
Laddge <dev.laddge@gmail.com>
2025-06-21 20:54:21 +0900

    feat: COMポート選択とバージョン表示を実装

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,67 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use crc32fast::Hasher;
+use cobs;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn encode_packet(input: Vec<u8>) -> Vec<u8> {
+    let mut hasher = Hasher::new();
+    hasher.update(&input);
+    let crc = hasher.finalize();
+
+    let mut data_with_crc = input.clone();
+    data_with_crc.extend_from_slice(&crc.to_be_bytes());
+
+    let mut encoded = cobs::encode_vec(&data_with_crc);
+    encoded.push(0x00);
+    return encoded;
 }
+
+#[tauri::command]
+fn decode_packet(input: Vec<u8>) -> Vec<u8> {
+    let mut fallback = input.clone();
+    if fallback.len() > 1 {
+        fallback[1] = 0x02;
+    }
+
+    let data = if let Some((&0x00, rest)) = input.split_last() {
+        rest
+    } else {
+        &input[..]
+    };
+
+    let decoded = match cobs::decode_vec(data) {
+        Ok(d) => d,
+        Err(_) => return fallback,
+    };
+
+    if decoded.len() < 4 {
+        return fallback;
+    }
+
+    let (payload, crc_bytes) = decoded.split_at(decoded.len() - 4);
+    let received_crc = u32::from_be_bytes(crc_bytes.try_into().unwrap());
+
+    let mut hasher = crc32fast::Hasher::new();
+    hasher.update(payload);
+    let calculated_crc = hasher.finalize();
+
+    if received_crc == calculated_crc {
+        payload.to_vec()
+    } else {
+        let mut fallback2 = decoded.clone();
+        if fallback2.len() > 1 {
+            fallback2[1] = 0x03;
+        }
+        return fallback2;
+    }
+}
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_serialplugin::init())
+        .invoke_handler(tauri::generate_handler![encode_packet, decode_packet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,101 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { invoke } from '@tauri-apps/api/core'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Icon } from '@iconify/vue'
 import { useColorMode } from '@vueuse/core'
+import { SerialPort } from 'tauri-plugin-serialplugin'
+import { invoke } from '@tauri-apps/api/core'
 
-const greetMsg = ref('')
-const name = ref('')
-const mode = useColorMode()
+const availabeVersions = [0x20]
 
-async function greet() {
-  greetMsg.value = await invoke('greet', { name: name.value })
-}
+useColorMode()
+const availablePorts = ref<string[]>([])
+const port = ref('')
+const loading = ref(0)
+const version = ref<number | null>(null)
+const versionStr = computed(() => (
+  version.value === null ? '' : `v${Math.floor(version.value / 16)}.${version.value % 16}`
+))
+
+let intervalId
+onMounted(() => {
+  intervalId = setInterval(async () => {
+    availablePorts.value = Object.entries(await SerialPort.available_ports()).filter(([k, v]) => v.vid === '12346' && v.pid == '4097').map(kv => kv[0]).sort()
+    if (availablePorts.value.length) {
+      if (!availablePorts.value.includes(port.value)) {
+        port.value = availablePorts.value[0]
+      }
+    } else {
+      port.value = ''
+    }
+  }, 1000)
+})
+
+onUnmounted(() => clearInterval(intervalId))
+
+watch(port, async (newPort) => {
+  version.value = null
+  if (newPort) {
+    loading.value = 1
+    const serial = new SerialPort({
+      path: newPort,
+      baudRate: 115200
+    })
+    try {
+      await serial.open()
+      const reqPacket = await invoke('encode_packet', { input: [0x00, 0x00] })
+      await serial.writeBinary(reqPacket)
+      const resData = await serial.readBinary({ timeout: 1000, size: 2100 })
+      const resPacket = await invoke('decode_packet', { input: resData })
+      if (resPacket[1] === 0x00 && availabeVersions.includes(resPacket[0])) {
+        version.value = resPacket[0]
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      loading.value = 0
+      await serial.close()
+    }
+  }
+})
 </script>
 
 <template>
   <main class="container mx-auto p-6">
-    <h1>Welcome to Tauri + Vue</h1>
-
-    <div class="flex">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="h-8 w-8" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="h-8 w-8" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="h-8 w-8" alt="Vue logo" />
-      </a>
+    <h1 class="font-bold text-4xl mb-4">NeoLEDitor</h1>
+    <div class="flex items-center">
+      ポートを選択:
+      <div class="px-4">
+        <Select v-model="port">
+          <SelectTrigger>
+            <SelectValue placeholder="ポートを選択" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectLabel>ポートを選択</SelectLabel>
+            <SelectGroup>
+              <SelectItem v-for="p in availablePorts" :key="p" :value="p">
+                {{ p }}
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <div v-if="port && !loading && version !== null" class="flex">
+        <Icon icon="lucide:circle-check" height="24" class="text-green-500 mr-1" />
+        {{ versionStr }}
+      </div>
+      <div v-if="port && !loading && version === null" class="flex text-red-500 font-bold">
+        <Icon icon="lucide:circle-x" height="24" class="mr-1" />
+        非対応デバイス
+      </div>
     </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <form class="row" @submit.prevent="greet">
-      <Input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <Button type="submit" class="cursor-pointer">Greet</Button>
-    </form>
-    <p>{{ greetMsg }}</p>
   </main>
 </template>
