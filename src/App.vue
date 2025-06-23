215388ad7214e6a823ef8ff38802a71e0e78a518
Laddge <dev.laddge@gmail.com>
2025-06-24 04:35:08 +0900

    feat: マッピング編集画面を実装

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import { Button } from '@/components/ui/button'
+import { Slider } from '@/components/ui/slider'
 import { Icon } from '@iconify/vue'
+import Mapping from '@/components/Mapping.vue'
 import { useColorMode } from '@vueuse/core'
 import { SerialPort } from 'tauri-plugin-serialplugin'
 import { invoke } from '@tauri-apps/api/core'
@@ -24,11 +27,22 @@ const version = ref<number | null>(null)
 const versionStr = computed(() => (
   version.value === null ? '' : `v${Math.floor(version.value / 16)}.${version.value % 16}`
 ))
+const cache = ref<number[]>([])
+const data = ref<number[]>([])
+const brightness = computed({
+  get() {
+    return [data.value[0]]
+  },
+  set(newBrightness) {
+    data.value[0] = newBrightness[0]
+  },
+})
 
-let intervalId
+let intervalId: NodeJS.Timeout | null = null
 onMounted(() => {
   intervalId = setInterval(async () => {
-    availablePorts.value = Object.entries(await SerialPort.available_ports()).filter(([k, v]) => v.vid === '12346' && v.pid == '4097').map(kv => kv[0]).sort()
+    const ports = await SerialPort.available_ports() as Record<string, { vid: string; pid: string }>
+    availablePorts.value = Object.entries(ports).filter(([_, v]) => v.vid === '12346' && v.pid == '4097').map(kv => kv[0]).sort()
     if (availablePorts.value.length) {
       if (!availablePorts.value.includes(port.value)) {
         port.value = availablePorts.value[0]
@@ -39,10 +53,16 @@ onMounted(() => {
   }, 1000)
 })
 
-onUnmounted(() => clearInterval(intervalId))
+onUnmounted(() => {
+  if (intervalId) {
+    clearInterval(intervalId)
+  }
+})
 
 watch(port, async (newPort) => {
   version.value = null
+  cache.value = []
+  data.value = []
   if (newPort) {
     loading.value = 1
     const serial = new SerialPort({
@@ -51,27 +71,57 @@ watch(port, async (newPort) => {
     })
     try {
       await serial.open()
-      const reqPacket = await invoke('encode_packet', { input: [0x00, 0x00] })
+      const reqPacket = await invoke('encode_packet', { input: [0x00, 0x00] }) as number[]
       await serial.writeBinary(reqPacket)
       const resData = await serial.readBinary({ timeout: 1000, size: 2100 })
-      const resPacket = await invoke('decode_packet', { input: resData })
+      const resPacket = await invoke('decode_packet', { input: resData }) as number[]
       if (resPacket[1] === 0x00 && availabeVersions.includes(resPacket[0])) {
         version.value = resPacket[0]
+        cache.value = resPacket.slice(2)
+        data.value = resPacket.slice(2)
       }
-    } catch (error) {
-      console.error(error)
+    } catch (err) {
+      console.error(err)
     } finally {
       loading.value = 0
       await serial.close()
     }
   }
 })
+
+async function save() {
+  const serial = new SerialPort({
+    path: port.value,
+    baudRate: 115200
+  })
+  try {
+    await serial.open()
+    const reqPacket = await invoke('encode_packet', { input: [version.value, 0x01, ...data.value] }) as number[]
+    await serial.writeBinary(reqPacket)
+    while (await serial.bytesToWrite() > 0) {
+      await new Promise(resolve => setTimeout(resolve, 1));
+    }
+    const resData = await serial.readBinary({ timeout: 1000, size: 2100 })
+    const resPacket = await invoke('decode_packet', { input: resData }) as number[]
+    if (resPacket[1] === 0x00 && availabeVersions.includes(resPacket[0])) {
+      cache.value = Array.from(data.value)
+    }
+  } catch (err) {
+    console.error(err)
+  } finally {
+    await serial.close()
+  }
+}
+
+function reset() {
+  data.value = Array.from(cache.value)
+}
 </script>
 
 <template>
-  <main class="container mx-auto p-6">
+  <main class="max-w-[1328px] mx-auto p-6 select-none">
     <h1 class="font-bold text-4xl mb-4">NeoLEDitor</h1>
-    <div class="flex items-center">
+    <div class="flex items-center mb-4">
       ポートを選択:
       <div class="px-4">
         <Select v-model="port">
@@ -97,5 +147,20 @@ watch(port, async (newPort) => {
         非対応デバイス
       </div>
     </div>
+    <template v-if="data.length">
+      <div class="mb-4 flex">
+        <Button @click="save" :disabled="JSON.stringify(cache) === JSON.stringify(data)" class="cursor-pointer mr-2">保存</Button>
+        <Button variant="secondary" @click="reset" :disabled="JSON.stringify(cache) === JSON.stringify(data)" class="cursor-pointer">リセット</Button>
+      </div>
+      <div class="mb-4 flex">
+        明るさ:
+        <Slider :min="0" :max="255" :step="1" v-model="brightness" class="w-60 mx-4" />
+        {{ data[0] }}
+      </div>
+      <div v-if="false" v-for="(_, i) in data.slice(1)" :key="i">
+        <input type="number" v-model="data[i + 1]" />
+      </div>
+      <Mapping v-model="data" />
+    </template>
   </main>
 </template>
---
+++ src/components/Mapping.vue
@@ -0,0 +1,264 @@
+<script setup lang="ts">
+import { ref, computed, onMounted, watch } from 'vue'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Icon } from '@iconify/vue'
+
+type MappingValue = [number, number]
+type MappingData = Record<string, MappingValue>
+
+const bgColors = ['bg-orange-500', 'bg-yellow-500', 'bg-green-500', 'bg-teal-500', 'bg-sky-500', 'bg-indigo-500', 'bg-purple-500', 'bg-pink-500']
+
+const data = defineModel<number[]>({ default: [...Array(2041)] })
+const appendStart = ref<number>(1)
+const appendEnd = ref<number>(2)
+const appendCount = ref<number>(1)
+const cursor = ref<number>(0)
+const cursorErr = ref<boolean>(false)
+const mapping = ref<MappingData>({})
+const selection = ref<MappingData>({})
+const dragStartCursor = ref<number>(0)
+const draging = ref<MappingData>({})
+const dragingTpl = computed(() => {
+  const tmp: number[] = []
+  const offset = cursor.value - Math.min(...Object.keys(draging.value).map(c => Number(c)))
+  for (const ci of Object.keys(draging.value).map(c => Number(c))) {
+    tmp.push(...[ci + offset, ci + 1 + offset, ci + 2 + offset])
+  }
+  const unavailable = Object.keys(mapping.value).map(c => [Number(c), Number(c) + 1, Number(c) + 2]).flat()
+  const intersection = tmp.filter(x => unavailable.includes(x))
+  cursorErr.value = intersection.length > 0
+  return tmp
+})
+
+let lock = false
+watch(data, (newData) => {
+  if (lock) return
+  lock = true
+  mapping.value = {}
+  let c = 1
+  while (c <= 510) {
+    const start = (newData[4 * (c - 1) + 1] << 8) + newData[4 * (c - 1) + 2]
+    const end = (newData[4 * (c - 1) + 3] << 8) + newData[4 * (c - 1) + 4]
+    if (start * end) {
+      mapping.value[c.toString()] = [start, end]
+      c += 3
+    } else {
+      c++
+    }
+  }
+  lock = false
+}, { immediate: true })
+watch(mapping, (newMapping) => {
+  if (lock) return
+  lock = true
+  for (let c = 1; c <= 510; c++) {
+    if (Object.keys(newMapping).includes(c.toString())) {
+      data.value[4 * (c - 1) + 1] = (newMapping[c.toString()][0] & 0xff00) >> 8
+      data.value[4 * (c - 1) + 2] = newMapping[c.toString()][0] & 0xff
+      data.value[4 * (c - 1) + 3] = (newMapping[c.toString()][1] & 0xff00) >> 8
+      data.value[4 * (c - 1) + 4] = newMapping[c.toString()][1] & 0xff
+    } else {
+      data.value[4 * (c - 1) + 1] = 0
+      data.value[4 * (c - 1) + 2] = 0
+      data.value[4 * (c - 1) + 3] = 0
+      data.value[4 * (c - 1) + 4] = 0
+    }
+  }
+  lock = false
+}, { deep: true })
+
+onMounted(() => {
+  document.addEventListener('click', outClick)
+  document.addEventListener('keydown', keydown)
+})
+
+function outClick(e: Event) {
+  const target = e.target as Element
+  if (!target.closest('.selectable')) {
+    selection.value = {}
+  }
+}
+
+function keydown(e: KeyboardEvent) {
+  if (e.ctrlKey || e.metaKey) {
+    if (e.code === 'KeyA') {
+      selection.value = JSON.parse(JSON.stringify(mapping.value))
+      e.preventDefault()
+    }
+  } else {
+    if (e.code === 'Delete') {
+      for (const c of Object.keys(selection.value)) {
+        delete selection.value[c]
+        delete mapping.value[c]
+      }
+    }
+    if (e.code === 'Escape') {
+      for (const c of Object.keys(selection.value)) {
+        delete selection.value[c]
+      }
+    }
+  }
+}
+
+function mousemove(e: MouseEvent) {
+  const target_rect = (e.currentTarget as Element).getBoundingClientRect()
+	const x = e.clientX - target_rect.left
+	const y = e.clientY - target_rect.top
+  cursor.value = Math.min(Math.floor(x / 40) + 1 + Math.floor(y / 40) * 32, 510)
+  if (dragStartCursor.value > 0 && cursor.value !== dragStartCursor.value && Object.keys(selection.value).length) {
+    (e.currentTarget as Element).classList.add('cursor-grabbing')
+    draging.value = selection.value
+    for (const c of Object.keys(draging.value)) {
+      delete mapping.value[c]
+    }
+  }
+}
+
+function mouseup(e: MouseEvent) {
+  (e.currentTarget as Element).classList.remove('cursor-grabbing')
+  if (!cursorErr.value) {
+    const offset = cursor.value - Math.min(...Object.keys(draging.value).map(c => Number(c)))
+    for (const c of Object.keys(draging.value)) {
+      const ci = Number(c)
+      mapping.value[(ci + offset).toString()] = draging.value[c]
+      selection.value[(ci + offset).toString()] = draging.value[c]
+      delete selection.value[c]
+    }
+  } else {
+    for (const c of Object.keys(draging.value)) {
+      if (Number(c) > 0) {
+        mapping.value[c] = draging.value[c]
+      }
+    }
+  }
+  draging.value = {}
+  dragStartCursor.value = 0
+}
+
+function dragstart(e: MouseEvent, c: string) {
+  if (!Object.keys(selection.value).includes(c)) {
+    if (!e.ctrlKey && !e.metaKey) {
+      selection.value = {}
+    }
+    selection.value[c] = mapping.value[c]
+  }
+  dragStartCursor.value = cursor.value
+}
+
+function append(e: Event) {
+  if (
+    1 <= appendStart.value && appendStart.value <= 65535 &&
+    1 <= appendEnd.value && appendEnd.value <= 65535 &&
+    1 <= appendCount.value && appendCount.value <= 170
+    ) {
+    for (let c = - (appendCount.value * 3); c < 0; c += 3) {
+      draging.value[c.toString()] = [appendStart.value, appendEnd.value]
+      const step = appendEnd.value - appendStart.value + 1
+      appendStart.value += step
+      appendEnd.value += step
+    }
+    appendCount.value = 1
+    ;(e.currentTarget as HTMLElement).blur()
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div id="appendAction">
+      <Button variant="secondary" class="cursor-pointer">
+        <Icon icon="lucide:plus" height="24" />
+      </Button>
+      <div tabindex="0" class="absolute z-100 mt-2 text-black h-0 overflow-hidden opacity-0 transition">
+        <Card class="w-[350px] shadow-md border-2 border-primary/75">
+          <CardContent>
+            <div class="mb-2">LEDアドレス</div>
+            <div class="grid grid-cols-5 w-full mb-4">
+              <Input type="number" min="1" max="65535" v-model="appendStart" class="col-span-2 text-right" />
+              <div class="text-center">~</div>
+              <Input type="number" min="1" max="65535" v-model="appendEnd" class="col-span-2 text-right" />
+            </div>
+            <div class="flex items-center">
+              <div class="flex-none mr-2">複製:</div>
+              <Input type="number" min="1" max="170" v-model="appendCount" class="w-16 text-right" />
+            </div>
+          </CardContent>
+          <CardFooter class="flex justify-end px-6">
+            <Button @click="append" class="cursor-pointer">追加</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+    <div class="overflow-auto py-2">
+      <div @mousemove="mousemove" @mouseup="mouseup" class="relative text-xs font-mono" :style="`width: ${40 * 32}px; height: ${40 * 16}px;`">
+        <div v-for="i in [...Array(512).keys()]" :class="`absolute p-1 select-none ${dragingTpl.includes(i + 1) && cursorErr ? 'z-100' : ''}`" :style="`width: 40px; height: 40px; top: ${40 * Math.floor(i / 32)}px; left: ${40 * (i % 32)}px;`">
+          <div :class="`opacity-75 rounded flex justify-center items-center h-full ${dragingTpl.includes(i + 1) ? 'border' : ''} ${dragingTpl.includes(i + 1) && cursorErr ? 'border-red-500 border-2 bg-red-900' : 'bg-secondary text-secondary-foreground border-primary'}`">
+            {{ i + 1 }}
+          </div>
+        </div>
+        <div v-for="c in Object.keys(mapping)" :key="c" @mousedown="(e) => dragstart(e, c)" class="selectable">
+          <div v-if="Number(c) % 32 === 0">
+            <div class="absolute p-1 select-none" :style="`width: 40px; height: 40px; top: ${40 * Math.floor((Number(c) - 1) / 32)}px; left: ${40 * ((Number(c) - 1) % 32)}px;`">
+              <div :class="`${bgColors[(mapping[c][1] - mapping[c][0]) % bgColors.length]} ${Object.keys(selection).includes(c) ? 'border-l border-y border-primary' : ''} w-full h-full rounded-l flex items-center text-black font-bold px-2`">
+                {{ c }}
+              </div>
+            </div>
+            <div class="absolute p-1 select-none" :style="`width: ${40 * 2}px; height: 40px; top: ${40 * Math.floor(Number(c) / 32)}px; left: ${40 * (Number(c) % 32)}px;`">
+              <div :class="`${bgColors[(mapping[c][1] - mapping[c][0]) % bgColors.length]} ${Object.keys(selection).includes(c) ? 'border-r border-y border-primary' : ''} w-full h-full rounded-r flex justify-end items-center text-black font-bold px-2`">
+                <div>
+                  <span class="inline-block">{{ mapping[c][0] }}~</span><span class="inline-block">{{ mapping[c][1] }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div v-else-if="Number(c) % 32 === 31">
+            <div class="absolute p-1 select-none" :style="`width: ${40 * 2}px; height: 40px; top: ${40 * Math.floor((Number(c) - 1) / 32)}px; left: ${40 * ((Number(c) - 1) % 32)}px;`">
+              <div :class="`${bgColors[(mapping[c][1] - mapping[c][0]) % bgColors.length]} ${Object.keys(selection).includes(c) ? 'border-l border-y border-primary' : ''} w-full h-full rounded-l flex justify-between items-center text-black font-bold px-2`">
+                <div class="flex-none">
+                  {{ c }}
+                </div>
+                <div class="text-center ml-2">
+                  <span class="inline-block">{{ mapping[c][0] }}~</span><span class="inline-block">{{ mapping[c][1] }}</span>
+                </div>
+              </div>
+            </div>
+            <div class="absolute p-1 select-none" :style="`width: 40px; height: 40px; top: ${40 * Math.floor((Number(c) + 1) / 32)}px; left: ${40 * ((Number(c) + 1) % 32)}px;`">
+              <div :class="`${bgColors[(mapping[c][1] - mapping[c][0]) % bgColors.length]} ${Object.keys(selection).includes(c) ? 'border-r border-y border-primary' : ''} w-full h-full rounded-r`"></div>
+            </div>
+          </div>
+          <div v-else class="absolute p-1 select-none" :style="`width: ${40 * 3}px; height: 40px; top: ${40 * Math.floor((Number(c) - 1) / 32)}px; left: ${40 * ((Number(c) - 1) % 32)}px;`">
+            <div :class="`${bgColors[(mapping[c][1] - mapping[c][0]) % bgColors.length]} ${Object.keys(selection).includes(c) ? 'border border-primary' : ''} w-full h-full rounded flex justify-between items-center text-black font-bold px-2`">
+              <div class="flex-none">
+                {{ c }}
+              </div>
+              <div>
+                <span class="inline-block">{{ mapping[c][0] }}~</span><span class="inline-block">{{ mapping[c][1] }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#appendAction:focus-within div {
+  height: auto;
+  opacity: 1;
+}
+input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+input[type=number] {
+  appearance: textfield;
+}
+</style>
